744620bb50479cbccf689986722ae074f52b795f
DanielaReinhardt <99242852+DanielaReinhardt@users.noreply.github.com>
2024-01-30 14:09:43 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import logging
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    print("Hello World")
+    logging.info("Hello World was printed")
+
+if __name__ == "__main__":
+    main()
